26934cd214b9c67ecab67c4be1cb6a5d6322f76a
Alexander <ya.kotik666@yandex.ru>
2018-09-06 05:13:52 +0300

    Added launching animation of the bot, beautify js

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,74 @@
    *  .status-bar__time | Отображение актуального времени на iPhone X
    */
   $('.status-bar__time time').ready(function() {
-    const [ hours, minutes ] = new Date().toLocaleTimeString().split(':');
-    $('.status-bar__time time').text(hours + ':' + minutes);
+    $('.status-bar__time time').text(("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2));
     $('.status-bar__time').css("background-position", "87.5% 79%");
 
     setInterval(function() {
-      const [ hours, minutes ] = new Date().toLocaleTimeString().split(':');
-      $('.status-bar__time time').text(hours + ':' + minutes);
+      $('.status-bar__time time').text(("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2));
     }, 2000);
   });
+
+  /**
+   *  Нажатие на кнопку '< Back'
+   */
+  $(function() {
+    $('.top-tg-bar__back').on('click', function() {
+      $('main').addClass('animated flash fast');
+      $('.top-tg-bar__back').addClass('disabled');
+      setTimeout(() => {
+        $('main').removeClass('animated flash fast');
+        $('.top-tg-bar__back').removeClass('disabled');
+      }, 900);
+    });
+  });
+
+  /**
+   *  Публикация сообщения
+   */
+  let top = 138;
+  function postMessage(className, message) {
+    const article = document.createElement('article');
+    article.innerHTML = `<p class="${className}">${message}</p><div style="clear:both"></div>`;
+    $(article).addClass('animated fadeInUp faster');
+    $('#chat').append(article);
+
+    const articleHeight = Math.round(parseFloat($(article).outerHeight(true)) * 1000) / 1000,
+            mainHeight  = Math.round(parseFloat($('#chat').outerHeight()) * 1000) / 1000,
+            percent     = ((articleHeight / mainHeight) * 100) * 1.431;
+    top -= percent;
+
+    if (top > 0) {
+      $('article:first-of-type').css('margin-top', top + '%');
+    } else {
+      $(article).removeClass('animated fadeInUp faster');
+      $('article').css('transition', 'none');
+      $('article:first-of-type').css('margin-top', '2%');
+      $(article).get(0).scrollIntoView({ block: 'end',  behavior: 'smooth' });
+    }
+
+    $('footer>nav>ul>li').css('pointer-events', 'none');
+    setTimeout(function() {
+      $('footer>nav>ul>li').css('pointer-events', 'auto');
+    }, 500);
+  }
 
   /**
    *  Инициализация бота: отправка сообщения '/start', отображение навигации бота
    */
   $(function() {
-    let top = 138;
-    const MAGIC_RATIO = 1.431;
+    (function printLetters(chars) {
+        document.getElementById('text-message').value += chars.shift();
+        if (chars.length) {
+            setTimeout(printLetters, 200, chars);
+        }
+    })('/start'.split(''));
+
     setTimeout(function() {
+      $("#text-message").prop('disabled', false);
+      $('.top-tg-bar__back').removeClass('disabled');
+      document.getElementById('text-message').value = '';
+
       const article = document.createElement('article');
       article.innerHTML = '<p class="user-text">/start</p><div style="clear:both"></div>';
       $('#chat').append(article);
@@ -33,39 +84,19 @@
       setTimeout(function() {
         $('footer>nav>ul>li').addClass('animated jackInTheBox fast');
         $('footer>nav>ul>li').css('display', 'block');
-      }, 200);
-    }, 800);
+        postMessage('bot-text', 'Погоди-ка...');
+      }, 800);
+    }, 1800);
 
     /**
      *  Запрос на получение информации обо мне
      */
     $('li#about-me').on('click', function() {
       $('article').css('transition', 'all 0.1s linear');
-
-      const article = document.createElement('article');
-      article.innerHTML = `<p class="user-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam sed eius totam ducimus consequatur, illo nobis rerum, placeat soluta magni ipsum sapiente expedita pariatur libero commodi laborum! Voluptatum, eveniet!</p><div style="clear:both"></div>`;
-      $(article).addClass('animated fadeInUp faster');
-      $('#chat').append(article);
-
-      const articleHeight = Math.round(parseFloat($(article).outerHeight(true)) * 1000) / 1000,
-            mainHeight    = Math.round(parseFloat($('#chat').outerHeight()) * 1000) / 1000,
-            percent       = ((articleHeight / mainHeight) * 100) * MAGIC_RATIO;
-      top -= percent;
-
-      if (top > 0) {
-        $('article:first-of-type').css('margin-top', top + '%');
-      } else {
-        $(article).removeClass('animated fadeInUp faster');
-        $('article').css('transition', 'none');
-        $('article:first-of-type').css('margin-top', '2%');
-        $(article).get(0).scrollIntoView({ block: 'end',  behavior: 'smooth' });
-      }
-
-      $('footer>nav>ul>li').css('pointer-events', 'none');
-      const timerId = setInterval(function() {
-        $('footer>nav>ul>li').css('pointer-events', 'auto');
-        clearInterval(timerId);
-      }, 400);
+      postMessage('user-text', 'Расскажи-ка о себе <img src="../img/thinking-face.png";">');
+      setTimeout(function() {
+        postMessage('bot-text', 'Погоди-ка...');
+      }, 800);
     });
   });
 })(jQuery);
--- src/js/transpiled-script.js
+++ src/js/transpiled-script.js
@@ -1,42 +1,81 @@
 "use strict";
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 !function ($, undefined) {
   /**
    *  .status-bar__time | Отображение актуального времени на iPhone X
    */
   $('.status-bar__time time').ready(function () {
-    var _toLocaleTimeString$s = new Date().toLocaleTimeString().split(':'),
-        _toLocaleTimeString$s2 = _slicedToArray(_toLocaleTimeString$s, 2),
-        hours = _toLocaleTimeString$s2[0],
-        minutes = _toLocaleTimeString$s2[1];
-
-    $('.status-bar__time time').text(hours + ':' + minutes);
+    $('.status-bar__time time').text(("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2));
     $('.status-bar__time').css("background-position", "87.5% 79%");
     setInterval(function () {
-      var _toLocaleTimeString$s3 = new Date().toLocaleTimeString().split(':'),
-          _toLocaleTimeString$s4 = _slicedToArray(_toLocaleTimeString$s3, 2),
-          hours = _toLocaleTimeString$s4[0],
-          minutes = _toLocaleTimeString$s4[1];
-
-      $('.status-bar__time time').text(hours + ':' + minutes);
+      $('.status-bar__time time').text(("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2));
     }, 2000);
   });
+  /**
+   *  Нажатие на кнопку '< Back'
+   */
+
+  $(function () {
+    $('.top-tg-bar__back').on('click', function () {
+      $('main').addClass('animated flash fast');
+      $('.top-tg-bar__back').addClass('disabled');
+      setTimeout(function () {
+        $('main').removeClass('animated flash fast');
+        $('.top-tg-bar__back').removeClass('disabled');
+      }, 900);
+    });
+  });
+  /**
+   *  Публикация сообщения
+   */
+
+  var top = 138;
+
+  function postMessage(className, message) {
+    var article = document.createElement('article');
+    article.innerHTML = "<p class=\"" + className + "\">" + message + "</p><div style=\"clear:both\"></div>";
+    $(article).addClass('animated fadeInUp faster');
+    $('#chat').append(article);
+    var articleHeight = Math.round(parseFloat($(article).outerHeight(true)) * 1000) / 1000,
+        mainHeight = Math.round(parseFloat($('#chat').outerHeight()) * 1000) / 1000,
+        percent = articleHeight / mainHeight * 100 * 1.431;
+    top -= percent;
+
+    if (top > 0) {
+      $('article:first-of-type').css('margin-top', top + '%');
+    } else {
+      $(article).removeClass('animated fadeInUp faster');
+      $('article').css('transition', 'none');
+      $('article:first-of-type').css('margin-top', '2%');
+      $(article).get(0).scrollIntoView({
+        block: 'end',
+        behavior: 'smooth'
+      });
+    }
+
+    $('footer>nav>ul>li').css('pointer-events', 'none');
+    setTimeout(function () {
+      $('footer>nav>ul>li').css('pointer-events', 'auto');
+    }, 500);
+  }
   /**
    *  Инициализация бота: отправка сообщения '/start', отображение навигации бота
    */
 
+
   $(function () {
-    var top = 138;
-    var MAGIC_RATIO = 1.431;
+    (function printLetters(chars) {
+      document.getElementById('text-message').value += chars.shift();
+
+      if (chars.length) {
+        setTimeout(printLetters, 200, chars);
+      }
+    })('/start'.split(''));
+
     setTimeout(function () {
+      $("#text-message").prop('disabled', false);
+      $('.top-tg-bar__back').removeClass('disabled');
+      document.getElementById('text-message').value = '';
       var article = document.createElement('article');
       article.innerHTML = '<p class="user-text">/start</p><div style="clear:both"></div>';
       $('#chat').append(article);
@@ -48,40 +87,19 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
       setTimeout(function () {
         $('footer>nav>ul>li').addClass('animated jackInTheBox fast');
         $('footer>nav>ul>li').css('display', 'block');
-      }, 200);
-    }, 800);
+        postMessage('bot-text', 'Погоди-ка...');
+      }, 800);
+    }, 1800);
     /**
      *  Запрос на получение информации обо мне
      */
 
     $('li#about-me').on('click', function () {
       $('article').css('transition', 'all 0.1s linear');
-      var article = document.createElement('article');
-      article.innerHTML = "<p class=\"user-text\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam sed eius totam ducimus consequatur, illo nobis rerum, placeat soluta magni ipsum sapiente expedita pariatur libero commodi laborum! Voluptatum, eveniet!</p><div style=\"clear:both\"></div>";
-      $(article).addClass('animated fadeInUp faster');
-      $('#chat').append(article);
-      var articleHeight = Math.round(parseFloat($(article).outerHeight(true)) * 1000) / 1000,
-          mainHeight = Math.round(parseFloat($('#chat').outerHeight()) * 1000) / 1000,
-          percent = articleHeight / mainHeight * 100 * MAGIC_RATIO;
-      top -= percent;
-
-      if (top > 0) {
-        $('article:first-of-type').css('margin-top', top + '%');
-      } else {
-        $(article).removeClass('animated fadeInUp faster');
-        $('article').css('transition', 'none');
-        $('article:first-of-type').css('margin-top', '2%');
-        $(article).get(0).scrollIntoView({
-          block: 'end',
-          behavior: 'smooth'
-        });
-      }
-
-      $('footer>nav>ul>li').css('pointer-events', 'none');
-      var timerId = setInterval(function () {
-        $('footer>nav>ul>li').css('pointer-events', 'auto');
-        clearInterval(timerId);
-      }, 400);
+      postMessage('user-text', 'Расскажи-ка о себе <img src="../img/thinking-face.png";">');
+      setTimeout(function () {
+        postMessage('bot-text', 'Погоди-ка...');
+      }, 800);
     });
   });
 }(jQuery);
